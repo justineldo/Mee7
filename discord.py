1b1b3594f38961b487e2c682f6d398a5d4fa71ca
justineldo <90534286+justineldo@users.noreply.github.com>
2021-09-11 23:41:39 +0000

    Update discord.py

--- discord.py
+++ discord.py
@@ -3,12 +3,12 @@ from discord.ext import commands
 import discord
 import levels
 
-cogs = [levels] ##class system which allows modular design
+cogs = [levels]
 
 client = commands.Bot[command_prefix='.', intents = discord.Intents.all()] #initiating a client
 
-for i in range(len(cogs)): #to activate the cogs
+for i in range(len(cogs)):
     cogs[i].setup(client)
     print('Setup successful.')
     
-client.run('token here') #the discord bot token from developer portal goes here
+client.run('token here')
